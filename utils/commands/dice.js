422f5d779cd16f11d6eb79384a34081ceb684500
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2022-08-30 12:21:06 +0200

    Update and rename commands/dice.js to utils/commands/dice.js

---
+++ utils/commands/dice.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js'); 
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+function randomNumb(pNumber){
+    let RandomNumber = getRandomArbitrary(1,pNumber);
+    let RoundRandom = Math.floor(RandomNumber);
+    return RoundRandom;
+}
+
+
+
+module.exports = {
+    name : 'dice6',
+    description : "dice6 command",
+    run:(client, message, args) => {
+        message.channel.send(""+ randomNumb(6));
+        
+    },
+};
