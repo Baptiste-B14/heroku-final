a6716bf47dfec594393ac9fff203d83075c6266b
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2022-09-02 21:23:15 +0200

    Update randomNumb.js

--- commands/utils/randomNumb.js
+++ commands/utils/randomNumb.js
@@ -1,10 +1,11 @@
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
-  }
+  };
 
 function randomNumb(pNumber){
     let RandomNumber = getRandomArbitrary(1,pNumber);
     let RoundRandom = Math.floor(RandomNumber);
     return RoundRandom;
-}
+};
+
