6232216c103fe0485503064cf44f8a34b32709e0
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2022-09-03 01:42:45 +0200

    Update dice20.js

--- commands/utils/dice20.js
+++ commands/utils/dice20.js
@@ -1,6 +1,17 @@
 const { MessageEmbed } = require('discord.js'); 
 
-import{randomNumb} from './randomNumb.js';
+//import{randomNumb} from './randomNumb.js';
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  };
+
+ function randomNumb(pNumber){
+    let RandomNumber = getRandomArbitrary(1,pNumber);
+    let RoundRandom = Math.floor(RandomNumber);
+    return RoundRandom;
+};
+
 
 module.exports = {
     name : 'dice20',
