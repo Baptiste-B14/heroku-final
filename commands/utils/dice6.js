68c0123c1a2807f9d4f665491b82fe9300707f12
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2022-09-06 22:11:21 +0200

    Update dice6.js

--- commands/utils/dice6.js
+++ commands/utils/dice6.js
@@ -1,21 +1,25 @@
+
+
 const { MessageEmbed } = require('discord.js'); 
 
 //import{randomNumb} from './randomNumb.js';
 
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-  };
 
- function randomNumb(pNumber){
-    let RandomNumber = getRandomArbitrary(1,pNumber+1);
-    let RoundRandom = Math.floor(RandomNumber);
-    return RoundRandom;
-};
+
+
+
 module.exports = {
     name : 'dice6',
     description : "dice6 command",
     run:(client, message, args) => {
-        message.channel.send(""+ randomNumb(6));
+        
+        var a=0;
+		a=Math.random();
+		var value = 0;
+
+
+        value=Math.ceil(6*a);
+        message.channel.send(""+ value);
         
     },
 };
