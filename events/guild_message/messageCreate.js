f0d360b0adc3c684f36b3d06b9b8fe6f5584e0eb
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2022-10-17 22:58:46 +0200

    Update messageCreate.js

--- events/guild_message/messageCreate.js
+++ events/guild_message/messageCreate.js
@@ -1,16 +1,27 @@
-const prefix = '*';
+const { MessageEmbed } = require('discord.js'); 
+
+
 
 module.exports = {
-    name: "messageCreate",
-    once: false,
-    execute(client, message){
-        if(message.author.bot) return;
-        if(!message.content.startsWith(prefix)) return;
+    name : 'quote',
+    description : "return a quote of Star Wars",
+    run:(client, message) => {
+        
+        if(message.content.includes("hello there")){
+            message.channel.send("General Kenobi");
+            return;
+        }else if(message.content.includes("€") ||  message.content.includes("$")){
+            message.channel.send("I smell prooooofit !");
+            return;
 
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const cmdName = args.shift().toLowerCase();
-        if(cmdName.length == 0) return;
-        let cmd = client.commands.get(cmdName);
-        if(cmd) cmd.run(client, message, args);
+        }else if(message.content.includes("je peux") || message.content.includes("je veux") ){
+            message.channel.send("Be Careful Not to Choke on Your Aspirations, Director");
+            return;
+
+        }else if(message.content.toLowerCase().includes("for the republic") || message.content.includes("Pour la république !") ){
+            message.channel.send("FOR THE REPUBLIC !!!");
+            return; 
+        }else return;
+        
     },
 };
