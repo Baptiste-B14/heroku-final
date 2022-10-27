a51484831ed541d01b7aa38f60d980fa8f19e560
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2022-10-28 00:13:38 +0200

    Update messageCreate.js

--- events/guild_message/messageCreate.js
+++ events/guild_message/messageCreate.js
@@ -10,26 +10,26 @@ module.exports = {
         
         if(message.author.bot) return;
         //quote.run(client, message);
-        if(message.content.includes("hello there")){
-            message.channel.send("General Kenobi");
-            return;
-        }else if(message.content.includes("€") ||  message.content.includes("$")){
-            message.channel.send("I smell prooooofit !");
-            return;
-
-        }else if(message.content.includes("je peux") || message.content.includes("je veux") ){
-            message.channel.send("Be Careful Not to Choke on Your Aspirations, Director");
-            return;
-
-        }else if(message.content.toLowerCase().includes("for the republic") || message.content.includes("Pour la république !") ){
-            message.channel.send("FOR THE REPUBLIC !!!");
-            return; 
-        }else if(message.content.toLowerCase().includes("explosion") || message.content.toLowerCase().includes("exploser")){
-            message.channel.send("calmitude et sérénité, rien ne va exploser");
-        }else return;
         
-        if(message.content.startsWith(prefix)){
-           
+        if(!message.content.startsWith(prefix)){
+            if(message.content.includes("hello there")){
+                message.channel.send("General Kenobi");
+                return;
+            }else if(message.content.includes("€") ||  message.content.includes("$")){
+                message.channel.send("I smell prooooofit !");
+                return;
+
+            }else if(message.content.includes("je peux") || message.content.includes("je veux") ){
+                message.channel.send("Be Careful Not to Choke on Your Aspirations, Director");
+                return;
+
+            }else if(message.content.toLowerCase().includes("for the republic") || message.content.includes("Pour la république !") ){
+                message.channel.send("FOR THE REPUBLIC !!!");
+                return; 
+            }else if(message.content.toLowerCase().includes("explosion") || message.content.toLowerCase().includes("exploser")){
+                message.channel.send("calmitude et sérénité, rien ne va exploser");
+            }else return;
+        }else{          
             const args = message.content.slice(prefix.length).trim().split(/ +/g);
             const cmdName = args.shift().toLowerCase();
             if(cmdName.length == 0) return;
